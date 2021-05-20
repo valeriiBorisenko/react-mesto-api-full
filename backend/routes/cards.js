9b6e9aa46ab6f70fc1309a849ba6a0ced5ba3a51
Krotpeshehod <tjvell@mail.ru>
2021-05-20 18:00:40 +0300

    fix: url cards likes front and back

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,7 +10,7 @@ const cardsRoutes = express.Router();
 cardsRoutes.get('/', getCards);
 cardsRoutes.post('/', express.json(), joiCardData, createCard);
 cardsRoutes.delete('/:cardId', joiCardId, deleteCard);
-cardsRoutes.put('/likes/:cardId', joiCardId, likeCard);
-cardsRoutes.delete('/likes/:cardId', joiCardId, dislikeCard);
+cardsRoutes.put('/:cardId/likes', joiCardId, likeCard);
+cardsRoutes.delete('/:cardId/likes', joiCardId, dislikeCard);
 
 exports.cardsRoutes = cardsRoutes;
